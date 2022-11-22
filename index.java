5da2f0ba49fbf0304a82222d54a207bdac8519d1
Your Name <moisesjuniorcas20@gmail.com>
2022-11-22 15:11:23 -0500

    empezando a repasar java desde 0

---
+++ index.java
@@ -0,0 +1,10 @@
+public class index {
+    public static void main(String[] args) {
+        int a = 1; //declarar entero int 
+        System.out.println(a); //imprimir entero int 
+
+        a = 2; //asignar entero int
+        System.out.println(a); //imprimir entero int
+    }
+} 
+
