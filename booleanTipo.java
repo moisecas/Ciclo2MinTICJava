b70a3be5fc74bcfd429576952bbd3f474deafcae
Your Name <moisesjuniorcas20@gmail.com>
2022-12-12 16:49:50 -0500

    boolean

---
+++ booleanTipo.java
@@ -0,0 +1,26 @@
+public class booleanTipo {
+    public static void main (String[] args){
+        //declaración de variables
+        boolean varBoolean = true;
+        System.out.println("varBoolean = " + varBoolean);
+        boolean varBoolean2 = false;
+        System.out.println("varBoolean2 = " + varBoolean2); 
+
+        //valores de tipo bandera
+        if (varBoolean){ //si es verdadero
+            System.out.println("La bandera es verdadera");
+        } else { //si no es verdadero
+            System.out.println("La bandera es falsa");
+        }
+
+        //si una persona es mayor de edad
+        int edad = 19;
+        boolean esMayorDeEdad = edad >= 18; //asignacion de tipo expresion booleana
+        if (esMayorDeEdad){ //si es mayor de edad
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("Es menor de edad");
+        }
+    } 
+
+}
