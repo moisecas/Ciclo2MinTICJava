0b7906f937d19da4ace7143d2c8970b7df7941a3
Your Name <moisesjuniorcas20@gmail.com>
2023-01-08 22:25:28 -0500

    repaso java

--- conversiones.java
+++ conversiones.java
@@ -4,5 +4,9 @@ public class conversiones {
        var edaddos = Integer.parseInt(edad); //conversion de String a int
          System.out.println("edad = " + (edaddos + 1)); //se suma 1 a la edad
          System.out.println("var edad string se concatena"+ edad + 1); //se concatena 1 a la edad
+
+            var valorPI = "3.1416";
+            var valorPIDos = Double.parseDouble(valorPI); //conversion de String a double
+            System.out.println("valorPI = " + valorPIDos);
     }
 }
