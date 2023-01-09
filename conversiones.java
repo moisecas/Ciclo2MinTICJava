e7a1f5f08ca949796a875b5948f0b0eb2dac6835
Your Name <moisesjuniorcas20@gmail.com>
2023-01-08 22:28:34 -0500

    repaso java

--- conversiones.java
+++ conversiones.java
@@ -1,3 +1,8 @@
+
+import java.util.Scanner;
+
+
+
 public class conversiones {
     public static void main (String[] args){
        var edad = "20";
@@ -8,5 +13,17 @@ public class conversiones {
             var valorPI = "3.1416";
             var valorPIDos = Double.parseDouble(valorPI); //conversion de String a double
             System.out.println("valorPI = " + valorPIDos);
+
+            //pedir un valor por consola de tipo string y convertirlo a int para sumarlo
+            
+            System.out.println("Proporcione el valor de radio: ");
+            Scanner consola = new Scanner(System.in);
+            var radio = Double.parseDouble(consola.nextLine()); //conversion de String a double
+            var area = Math.PI * Math.pow(radio, 2);
+            System.out.println("area = " + area);
+
+            
+
+
     }
 }
